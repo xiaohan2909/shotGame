ab3bd262155e1f80a097294e7c2f6591558edb82
lixiaohan <415832014@qq.com>
2016-04-14 15:18:46 +0800

    提交第一版

---
+++ Assets/Scripts/RendererExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Renderer extensions. C#类扩展的用法
+/// </summary>
+public static class RendererExtensions
+{
+	public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
+	{
+		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+		return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+	}
+}
---
+++ Assets/Scripts/ShotScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotScript : MonoBehaviour {
+	// 1 - Designer variables
+	
+	/// <summary>
+	/// Damage inflicted
+	/// </summary>
+	public int damage = 1;
+	
+	/// <summary>
+	/// Projectile damage player or enemies?
+	/// </summary>
+	public bool isEnemyShot = false;
+	// Use this for initialization
+	void Start()
+	{
+		// 2 - Limited time to live to avoid any leak
+		Destroy(gameObject, 20); // 20sec
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
